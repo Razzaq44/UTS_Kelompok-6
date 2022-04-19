a9321c3f33e2b230e6fffc26909f7870ed243eee
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-04-19 21:32:02 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace GUI_Razzaq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Judul = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Pesan = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.nominal = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Rekening = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Judul);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // Judul
+            // 
+            this.Judul.AutoSize = true;
+            this.Judul.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Judul.ForeColor = System.Drawing.Color.White;
+            this.Judul.Location = new System.Drawing.Point(97, 12);
+            this.Judul.Name = "Judul";
+            this.Judul.Size = new System.Drawing.Size(102, 20);
+            this.Judul.TabIndex = 0;
+            this.Judul.Text = "Tarik Tunai";
+            this.Judul.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Judul.Click += new System.EventHandler(this.Judul_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 495);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 55);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(73, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tarik Tunai Sekarang";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.Pesan);
+            this.panel3.Location = new System.Drawing.Point(0, 299);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(300, 166);
+            this.panel3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(15, 44);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 82);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Pesan
+            // 
+            this.Pesan.AutoSize = true;
+            this.Pesan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pesan.Location = new System.Drawing.Point(12, 17);
+            this.Pesan.Name = "Pesan";
+            this.Pesan.Size = new System.Drawing.Size(113, 15);
+            this.Pesan.TabIndex = 3;
+            this.Pesan.Text = "Pesan (optional)";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.button7);
+            this.panel4.Controls.Add(this.button6);
+            this.panel4.Controls.Add(this.button5);
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.nominal);
+            this.panel4.Location = new System.Drawing.Point(0, 162);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(300, 130);
+            this.panel4.TabIndex = 3;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightGray;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(173, 73);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "5 jt";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightGray;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(92, 73);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "1 jt";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightGray;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(11, 73);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "500 rb";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGray;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(173, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "300 rb";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(92, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "200 rb";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(11, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "100 rb";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nominal
+            // 
+            this.nominal.AutoSize = true;
+            this.nominal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nominal.Location = new System.Drawing.Point(12, 17);
+            this.nominal.Name = "nominal";
+            this.nominal.Size = new System.Drawing.Size(170, 15);
+            this.nominal.TabIndex = 2;
+            this.nominal.Text = "Pilih Nominal Tarik Tunai";
+            this.nominal.Click += new System.EventHandler(this.nominal_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Controls.Add(this.textBox1);
+            this.panel5.Controls.Add(this.Rekening);
+            this.panel5.Location = new System.Drawing.Point(0, 56);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(300, 100);
+            this.panel5.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Rekening
+            // 
+            this.Rekening.AutoSize = true;
+            this.Rekening.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rekening.Location = new System.Drawing.Point(12, 17);
+            this.Rekening.Name = "Rekening";
+            this.Rekening.Size = new System.Drawing.Size(178, 15);
+            this.Rekening.TabIndex = 1;
+            this.Rekening.Text = "Masukan Rekening Tujuan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(300, 550);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label Judul;
+        private System.Windows.Forms.Label Pesan;
+        private System.Windows.Forms.Label nominal;
+        private System.Windows.Forms.Label Rekening;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GUI_Razzaq
+{
+    public partial class Form1 : Form
+    {
+        string rek;
+        int jumlah = 0;
+        string message;
+        
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Judul_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            rek = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            jumlah = 100000;            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            jumlah = 200000;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            jumlah = 300000;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            jumlah = 500000;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            jumlah = 1000000;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            jumlah = 5000000;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            message = textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WithdrawConfig withdraw = new WithdrawConfig();
+            int saldo = Convert.ToInt32(withdraw.config.saldo);
+            if (saldo <= 0)
+            {
+                MessageBox.Show("Saldo anda tidak mencukupi minimal penarikan tunai");
+                MessageBox.Show("Sisa saldo anda " + (saldo));
+            }
+            else
+            {
+                if (textBox1.Text == "" && jumlah == 0)
+                {
+                    MessageBox.Show("Rekening Tujuan Belum Diisi!");
+                    textBox1.Focus();                    
+                }
+                else
+                {
+                    int sisaSaldo = saldo - jumlah;
+                    if (sisaSaldo < 0)
+                    {
+                        MessageBox.Show("Saldo anda tidak mencukupi minimal penarikan tunai");
+                        MessageBox.Show("Sisa saldo anda " + (saldo));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tarik tunai ke rekening " + rek + " dengan nonimal Rp. " + jumlah + " berhasil");
+                        MessageBox.Show("Sisa saldo anda " + (saldo - jumlah));
+                        Config.UpdateSaldo(saldo, jumlah);
+                    }
+                    
+                }
+
+            }       
+            
+        }
+
+        private void nominal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class WithdrawConfig
+    {
+        public Config config;
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string fileConfigName = "withdraw_config.json";
+
+        public WithdrawConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        private Config ReadConfigFile()
+        {
+            string jsonStringFromFile = File.ReadAllText(path + "/" + fileConfigName);
+            config = System.Text.Json.JsonSerializer.Deserialize<Config>(jsonStringFromFile);
+            return config;
+        }
+
+        private void SetDefault()
+        {
+            int fee = 2500;
+
+            int saldo = 100000000;
+
+            config = new Config(fee, saldo);
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(config, options);
+            string fullFilePath = path + "/" + fileConfigName;
+            File.WriteAllText(fullFilePath, jsonString);
+        }
+    }
+
+    public class Config
+    {
+        
+        public int fee { get; set; }
+        public int saldo { get; set; }
+        public Config() { }
+        public Config(int fee, int saldo)
+        {
+            this.fee = fee;
+            this.saldo = saldo;
+        }
+        public static void UpdateSaldo(int Saldo, int Jumlah)
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string fileConfigName = "withdraw_config.json";
+            string pathAndFile = path + "/" + fileConfigName;
+
+            string jsonStringFromFile = File.ReadAllText(path + "/" + fileConfigName);
+            var config = System.Text.Json.JsonSerializer.Deserialize<Config>(jsonStringFromFile);
+
+            JObject jObject = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonStringFromFile) as JObject;
+            JToken jToken = jObject.SelectToken("saldo");
+            if (config.saldo <= 0)
+            {
+                int Newsaldo = 0;
+                jToken.Replace(Newsaldo);
+            }
+            else
+            {
+                int Newsaldo = Saldo - Jumlah - config.fee;
+                jToken.Replace(Newsaldo);
+            }
+            string updatedJsonString = jObject.ToString();
+            File.WriteAllText(pathAndFile, updatedJsonString);
+        }
+    }
+}
+
